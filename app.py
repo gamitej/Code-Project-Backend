8118841e7505e1d5aaceba5ea64baeaabb1cee9e
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-04 09:11:56 +0530

    logger added

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 # ======= Libs imports =========
 import atexit
 import sqlite3
+import logging
 from sqlite3 import dbapi2 as sqlite
 # ======= Flask imports ======== 
 from flask_cors import CORS
-from flask import Flask, jsonify
+from flask import Flask, jsonify,request
 from flask_limiter import Limiter
 from flask_compress import Compress
 from flask_limiter.util import get_remote_address
@@ -17,10 +18,33 @@ from routes.profile.profile import profile_routes
 from routes.auth.auth_db import AuthDb
 
 app = Flask(__name__)
+app.logger.setLevel(logging.INFO)
 limiter = Limiter(get_remote_address, app=app, default_limits=["200 per day", "50 per hour"])
 CORS(app)
 compress = Compress()
 compress.init_app(app)
+
+# Create a file handler for logging
+file_handler = logging.FileHandler('flask.log')
+
+# Set the log format
+formatter = logging.Formatter('%(asctime)s %(levelname)s: %(message)s - IP: %(client_ip)s')
+file_handler.setFormatter(formatter)
+
+# Add the file handler to the Flask logger
+app.logger.addHandler(file_handler)
+
+# Custom log filter to include client IP address
+class ClientIPFilter(logging.Filter):
+    def filter(self, record):
+        if hasattr(request, 'remote_addr'):
+            record.client_ip = request.remote_addr
+        else:
+            record.client_ip = 'Unknown'
+        return True
+
+# Apply the log filter to the Flask logger
+app.logger.addFilter(ClientIPFilter())
 
 # =============== Databse Connection Started ===============
 
@@ -36,6 +60,11 @@ connection = connect_to_db()
 authDbObj = AuthDb(connection)
 
 # =================== ROUTES START =========================
+
+# Log incoming requests before handling them
+@app.before_request
+def log_request():
+    app.logger.info(f"Incoming request: {request.method} {request.path}")
 
 
 app.register_blueprint(auth_routes(connection,limiter), url_prefix='/')
