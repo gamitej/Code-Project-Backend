88ad9dd97b29a92021a2807828959e0f5868cf50
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-10 19:45:59 +0530

    profile logic reimplemented

--- routes/profile/profile_db.py
+++ routes/profile/profile_db.py
@@ -12,9 +12,24 @@ class ProfileDatabase:
 
         json = {"easyTotal":0,"easySolved":0,"mediumTotal":0,"mediumSolved":0,"hardTotal":0,"hardSolved":0,"total":0,"totalSolved":0}
         if data is not None:
+            easyTotal,easySolved,mediumTotal,mediumSolved,hardTotal,hardSolved = 0,0,0,0,0,0
+            for i in range(len(data)):
+                if totalData[i][0] == "easy":
+                    easyTotal = totalData[i][1]
+                if data[i][0] == "easy":
+                    easySolved = data[i][1]
+                if totalData[i][0] == "medium":
+                    mediumTotal = totalData[i][1]
+                if data[i][0] == "medium":
+                    mediumSolved = data[i][1]
+                if totalData[i][0] == "hard":
+                    hardTotal = totalData[i][1]
+                if data[i][0] == "hard":
+                    hardSolved = data[i][1]
+
             total = totalData[0][1] + totalData[1][1] + totalData[2][1]
             totalSolved  = data[0][1] + data[1][1] + data[2][1] 
-            json = {"easyTotal":totalData[0][1],"easySolved":data[0][1],"mediumTotal":totalData[1][1],"mediumSolved":data[1][1],"hardTotal":totalData[2][1],"hardSolved":data[2][1],"total":total,"totalSolved":totalSolved}
+            json = {"easyTotal":easyTotal,"easySolved":easySolved,"mediumTotal":mediumTotal,"mediumSolved":mediumSolved,"hardTotal":hardTotal,"hardSolved":hardSolved,"total":total,"totalSolved":totalSolved}
         return json
 
 
