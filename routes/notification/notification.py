00204bb23d5eac3bdb69efdb3932064003369373
gamitej <gamitejpratapsingh998@gmail.com>
2023-07-19 16:01:30 +0530

    getnotifcation api done

--- routes/notification/notification.py
+++ routes/notification/notification.py
@@ -3,24 +3,27 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, create_access_token, get_jwt_identity
 # ======= File imports ===========
 from routes.database.database import data_base
-
+from routes.notification.notification_db import NotificationDb
 
 noti = Blueprint('noti', __name__)
 
 def notification_routes(connection,limiter):
     dataBaseObj = data_base(connection)
+    notiObj = NotificationDb(connection)
 
-    # =============================== LOGIN ============================
+    # =============================== GET NOTIFICATION ============================
 
     @noti.route('/notifications', methods=["GET"])
     @limiter.limit("30/minute")
     @jwt_required() 
     def getNotification():
         try:
-            return jsonify({"data": [{"id":1,"text":"tree added","seen":False},{"id":2,"text":"dp added","seen":True}],"error":False}), 200
+            id = request.args.get('id')
+            data = notiObj.getNotificationOfUser(id)
+            return jsonify({"data":data,"error":False}), 200
         except Exception as e:
             print(e)
-            return jsonify({"message": "Something went wrong","error":False}), 500
+            return jsonify({"message": "Something went wrong","error":True}), 500
     
     return noti
 
---
+++ routes/notification/notification_db.py
@@ -0,0 +1,26 @@
+from routes.database.database import data_base
+
+class NotificationDb:
+    table_name = "users"
+    key = ["id", "username", "password"]
+
+    def __init__(self,connection):
+        self.connection = connection
+        self.cursor = self.connection.cursor()
+        self.data_base_obj = data_base(connection)
+
+    def getNotificationOfUser(self,id):
+        query = f"SELECT n.notification_id, n.content,CASE WHEN un.user_id IS NULL THEN FALSE ELSE TRUE END AS seen FROM notifications n LEFT JOIN user_notification un ON n.notification_id = un.notification_id AND un.user_id = '{id}'"
+
+        data = self.data_base_obj.selectQuery(query,False)
+        jsonData = []
+        for row in data:
+            id,text,seen = row[0],row[1],row[2]
+            json = {"id":id, "text":text, "seen":seen}
+            jsonData.append(json)
+        return jsonData
+
+    
+
+if __name__=='__main__':
+    pass
